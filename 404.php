74fa6afa33ab10b27e79340cb1b67f1f523a9831
newworkakountmaxim <newworkakount@gmail.com>
2020-01-15 16:00:07 +0200

    Added 404- page

--- 404.php
+++ 404.php
@@ -10,51 +10,127 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<style>
+    .main-container-404 {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background-image: url("<?php echo get_template_directory_uri(); ?>/img/bg/bg-2.png");
+        background-size: cover;
+        background-position: center;
+    }
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'autoschool-mariupol' ); ?></h1>
-				</header><!-- .page-header -->
+    .block-404 {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        
+    }
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'autoschool-mariupol' ); ?></p>
+    .line {
+        width: 25px;
+        height: 227px;
+        background: rgb(0, 0, 0);
+    }
 
-					<?php
-					get_search_form();
+    .stop {
+        width: 335px;
+        height: 334px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: rgb(255, 255, 255);
+        border-radius: 180px;
+        box-shadow: 0px 3px 6px #00000029;
+        animation: main-rotate 0.9s cubic-bezier(0, 0, 0, 1);
+        transform: matrix(0.97, -0.22, 0.22, 0.97, 0, 0);
+    }
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+    .stop-inside-circle {
+        width: 300px;
+        height: 300px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: #D70000 0% 0% no-repeat padding-box;
+        border-radius: 180px;
+    }
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'autoschool-mariupol' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
+    .stop-inside-line {
+        width: 207px;
+        height: 45px;
+        background: #FFFFFF 0% 0% no-repeat padding-box;
+        border: 1px solid #707070;
+        border-radius: 25px;
+    }
 
-					<?php
-					/* translators: %1$s: smiley */
-					$autoschool_mariupol_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'autoschool-mariupol' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$autoschool_mariupol_archive_content" );
+    .block-text {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 40px;
+        letter-spacing: 0;
+        color: #000000;
+    }
+        .block-text p {
+                width: 50%;
+        }
+    .button-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+    .backward {
+        background: #D70000 0% 0% no-repeat padding-box;
+        box-shadow: 0px 3px 6px #00000029;
+        border-radius: 61px;
+        padding: 1% 3%;
+        font-family: 'Segoe UI';
+        letter-spacing: 2.4px;
+        color: #FFFFFF;
+    }
+    @keyframes main-rotate {
+    0% {
+        transform: rotateY(0deg);
+    }
+    10% {
+        transform: rotateY(360deg);
+    }
+    20% {
+        transform: rotateY(0deg);
+    }
+    100% {
+        transform: rotateY(360deg);
+    }
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+</style>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+<div class="main-container-404">
+    <div class="block-404">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/404/4.png" alt="4">
+        <div class="stop">
+            <div class="stop-inside-circle">
+                <div class="stop-inside-line">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                </div>
+            </div>
+        </div>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/404/4.png" alt="4">
+    </div>
+    <div class="block-text">
+        <p>Пожалуйста, будьте спокойны и вернитесь на предыдущую страницу</p>
+    </div>
+    <div class="button-container">
+        <div class="backward">
+            НАЗАД
+        </div>
+    </div>
+</div>
 
 <?php
 get_footer();
